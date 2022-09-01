e04a98bdfdc6d73cb95fbd6079dd14b4bba861c6
Henry <henryye2006@gmail.com>
2022-09-01 10:17:51 -0700

    started lab 02

---
+++ 002_BigO/base_code/pkg/BaseClass.java
@@ -0,0 +1,18 @@
+package pkg;
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class BaseClass {
+	public BaseClass() {
+		
+	}
+	public void randomize(int[]nums) {
+		int e;
+		for(int i = 0;i<nums[].length;i++) {
+			nums[i] = (int)(Math.random()*200000);
+			
+		}
+	}
+
+}
